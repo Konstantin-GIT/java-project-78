985870bac8c686b90edb08425f6057a88a953c5e
Konstantin Naumovich <konstantin000008@gmail.com>
2023-03-09 18:29:07 +0300

    added Validator class and test

---
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ValidatorTest {
+
+    @Test
+    public void testValidtor() {
+        assertEquals(1, 1);
+    }
+
+}
